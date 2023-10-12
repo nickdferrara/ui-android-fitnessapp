8216e878cc2da1054187fd776d304e2f16cf8c65
nickdferrara <nick.d.ferrara@gmail.com>
2023-10-12 19:25:44 -0400

    Make edge to edge

--- app/src/main/java/com/nickdferrara/ui_android_fitnessapp/MainActivity.kt
+++ app/src/main/java/com/nickdferrara/ui_android_fitnessapp/MainActivity.kt
@@ -2,7 +2,9 @@ package com.nickdferrara.ui_android_fitnessapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -16,6 +18,8 @@ import com.nickdferrara.ui_android_fitnessapp.ui.theme.UiandroidfitnessappTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdgeScreenLayout()
+
         setContent {
             UiandroidfitnessappTheme {
                 val navController = rememberNavController()
@@ -23,4 +27,19 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    private fun enableEdgeToEdgeScreenLayout() {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                android.graphics.Color.TRANSPARENT,
+                android.graphics.Color.TRANSPARENT
+            ),
+            navigationBarStyle = SystemBarStyle.light(
+                android.graphics.Color.TRANSPARENT,
+                android.graphics.Color.TRANSPARENT
+            )
+        )
+    }
 }
+
+
