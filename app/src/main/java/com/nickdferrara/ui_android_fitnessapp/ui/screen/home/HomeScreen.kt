1e1b6e0d47b9c88c1af6b2f709f9dc70f1fdb2f6
nickdferrara <nick.d.ferrara@gmail.com>
2023-10-12 23:11:51 -0400

    Make health connect dialog

--- app/src/main/java/com/nickdferrara/ui_android_fitnessapp/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/nickdferrara/ui_android_fitnessapp/ui/screen/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.nickdferrara.ui_android_fitnessapp.ui.screen.home
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -7,28 +8,40 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material3.AlertDialog
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.nickdferrara.ui_android_fitnessapp.R
+import java.text.SimpleDateFormat
+import java.util.Date
 
+@SuppressLint("SimpleDateFormat")
 @Composable
 fun HomeScreen(
     navController: NavController
 ) {
+    val sdf = SimpleDateFormat("EEEE, LLLL dd")
+    val currentDate = sdf.format(Date())
+
     Column(
         modifier = Modifier.padding(20.dp)
     ) {
@@ -38,13 +51,70 @@ fun HomeScreen(
                 .fillMaxWidth()
         ) {
             Text(
-                text = "Welcome Nicholas",
+                text = "Welcome Nicholas,",
                 style = MaterialTheme.typography.headlineMedium,
                 fontWeight = FontWeight.Bold
             )
             FitifyProfileImage(
                 drawableResource = R.drawable.profile_image,
                 description = "Profile Image"
+            )
+        }
+        Text(
+            text = "Today is $currentDate",
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.secondary
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        Card (
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.surfaceVariant,
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(210.dp)
+        )
+        {
+            Column(
+                modifier = Modifier.padding(20.dp)
+            ) {
+                Row {
+                    Icon(
+                        imageVector = Icons.Filled.Favorite,
+                        contentDescription = "Health Connect Icon"
+                    )
+                    Text(
+                        text = "Get more detailed workouts",
+                        modifier = Modifier.padding(start = 8.dp),
+                        fontWeight = FontWeight.Bold
+
+                    )
+                    Icon(
+                        imageVector = Icons.Filled.Clear,
+                        contentDescription = "Close Health Connect Icon"
+                    )
+                }
+                Text(
+                    text = "Keep Fitify updated with the latest information from your other apps, " +
+                            "like your calories, heart rate and body measurements.",
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(top = 16.dp)
+                )
+
+                TextButton(
+                    onClick ={},
+                    modifier = Modifier.padding(top = 4.dp)
+                ) {
+                    Text(text = "Set Up Health Connect" )
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(24.dp))
+        Row {
+            Text(
+                text = "Upcoming Workouts",
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.secondary
             )
         }
     }
