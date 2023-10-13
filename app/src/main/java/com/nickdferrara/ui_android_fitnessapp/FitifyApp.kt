f526a6c583260ab5a0ec9aadb44c6cd6a52e9118
nickdferrara <nick.d.ferrara@gmail.com>
2023-10-12 21:01:01 -0400

    Make dialog when device has no internet connection

--- app/src/main/java/com/nickdferrara/ui_android_fitnessapp/FitifyApp.kt
+++ app/src/main/java/com/nickdferrara/ui_android_fitnessapp/FitifyApp.kt
@@ -1,25 +1,79 @@
 package com.nickdferrara.ui_android_fitnessapp
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.navigation.NavHostController
 import com.nickdferrara.ui_android_fitnessapp.ui.navigation.BottomNavigationBar
 import com.nickdferrara.ui_android_fitnessapp.ui.navigation.NavigationGraph
 
+@SuppressLint("UnrememberedMutableState")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FitifyApp(
-    navController: NavHostController
+    navController: NavHostController,
 ) {
+    val context = LocalContext.current
+    var isOnline by mutableStateOf(checkIfOnline(context))
+
+    fun refreshOnline() {
+        isOnline = checkIfOnline(context)
+    }
+
     Scaffold(
         bottomBar = {
             BottomNavigationBar(navController = navController)
         }
     ) { innerPadding ->
-        NavigationGraph(
-            navController = navController,
-            innerPadding = innerPadding
-        )
+        if (isOnline) {
+            NavigationGraph(
+                navController = navController,
+                innerPadding = innerPadding
+            )
+        } else {
+            DisplayOfflineDialog { refreshOnline() }
+        }
     }
 }
+
+@Suppress("DEPRECATION")
+private fun checkIfOnline(context: Context): Boolean {
+    val cm = getSystemService(context, ConnectivityManager::class.java)
+
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        val capabilities = cm?.getNetworkCapabilities(cm.activeNetwork) ?: return false
+        capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
+                capabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+    } else {
+        cm?.activeNetworkInfo?.isConnectedOrConnecting == true
+    }
+}
+
+@Composable
+fun DisplayOfflineDialog(onRetry: () -> Unit) {
+    AlertDialog(
+        onDismissRequest = {},
+        title = { Text(text = stringResource(R.string.connection_error_title)) },
+        text = { Text(text = stringResource(R.string.connection_error_message)) },
+        confirmButton = {
+            TextButton(onClick = onRetry) {
+                Text(stringResource(R.string.retry_label))
+            }
+        }
+    )
+}
+
--- app/src/main/java/com/nickdferrara/ui_android_fitnessapp/MainActivity.kt
+++ app/src/main/java/com/nickdferrara/ui_android_fitnessapp/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import com.nickdferrara.ui_android_fitnessapp.ui.theme.UiandroidfitnessappTheme
 
@@ -19,6 +20,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             UiandroidfitnessappTheme {
                 val navController = rememberNavController()
