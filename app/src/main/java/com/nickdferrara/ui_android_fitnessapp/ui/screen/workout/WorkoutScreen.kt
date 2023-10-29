7788d3d2c91e98d3734deda4d0a84124b6712115
nickdferrara <nick.d.ferrara@gmail.com>
2023-10-28 23:40:13 -0400

    Make card block out for workout

--- app/src/main/java/com/nickdferrara/ui_android_fitnessapp/ui/screen/workout/WorkoutScreen.kt
+++ app/src/main/java/com/nickdferrara/ui_android_fitnessapp/ui/screen/workout/WorkoutScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -19,7 +20,9 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Place
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
@@ -34,10 +37,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.nickdferrara.ui_android_fitnessapp.R
 import com.nickdferrara.ui_android_fitnessapp.data.models.Workout
 import com.nickdferrara.ui_android_fitnessapp.util.findLastSeveDays
 import com.nickdferrara.ui_android_fitnessapp.util.findMockUpcomingWorkouts
@@ -102,12 +108,53 @@ fun WorkoutItem(workout: Workout) {
         Card(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(90.dp)
+                .height(130.dp)
                 .width(120.dp)
         ) {
+            Column(
+                verticalArrangement = Arrangement.spacedBy(6.dp),
+                modifier = Modifier.padding(10.dp),
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(10.dp),
+
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.barbell_icon),
+                        contentDescription = "Time Icon",
+                        tint = Color.White,
+                        modifier = Modifier
+                            .size(22.dp)
+
+                    )
+                    Text(
+                        text = "${workout.description}",
+                        style = MaterialTheme.typography.titleMedium,
+                        fontWeight = FontWeight.,
+                    )
+                }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(10.dp),
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.AccountCircle,
+                        contentDescription = "Time Icon",
+                        tint = Color.White,
+                        modifier = Modifier
+                            .size(22.dp)
+
+                    )
+                    Text(
+                        text = "${workout.coach}",
+                        style = MaterialTheme.typography.titleSmall,
+                        fontWeight = FontWeight.Bold,
+                    )
+                }
+            }
 
         }
-
     }
 }
 
